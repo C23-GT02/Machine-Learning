a9aa7efe4f3d22175f657b93b6f1347756b2bc14
fian39870 <fian39870@gmail.com>
2023-12-12 19:55:19 +0700

    perubahan API

--- app.py
+++ app.py
@@ -5,126 +5,58 @@ app = Flask(__name__)
 app.json.sort_keys = False
 db = firestore.Client.from_service_account_json('connection-firebase.json')
 
-@app.route('/api/<partner_name>/products', methods=['GET'])
-def get_product(partner_name):
+@app.route('/api/products', methods=['GET'])
+def get_product():
     try:
-        partner_ref = db.collection('verifiedPartner').document(partner_name)
-        partner_snapshot = partner_ref.get()
-        if not partner_snapshot.exists:
-            return jsonify({'status': 'failed', 'message': 'partner not found', 'data': None}), 404
-        products_collection_ref = partner_ref.collection('products')
-        products_snapshot = products_collection_ref.get()
+        product_ref = db.collection('scannedProducts').get()
+
+        if len(product_ref) == 0:
+            return jsonify({'status': 'failed', 'message': 'products not found', 'data': None}), 404
+
         products = []
-        for product in products_snapshot:
+        for product in product_ref:
             data = product.to_dict()
-            products.append(data)
+            user_ref = data.get('userRef')
+            user_doc = user_ref.get().to_dict()
+            user_data = {'id_user': user_doc.get('uuid')}
+            product_ref = data.get('productRef')
+            product_data = {}
+
+            if product_ref:
+                product_doc = product_ref.get().to_dict()
+
+                if product_doc:
+                    partner_ref_path = product_doc.get('partnerRef')
+                    partner_ref = db.document(partner_ref_path).get().to_dict()
+                    product_ref_path = product_doc.get('productRef')
+                    product_ref = db.document(product_ref_path).get().to_dict()
+                    product_data = {
+                        'id_produk': product_doc.get('id'),
+                        'Nama_Produk': product_doc.get('name'),
+                        'Harga' : product_ref.get('harga'),
+                        'Bahan baku' : product_ref.get('material'),
+                        'Kategori': product_ref.get('kategori') if 'kategori' in product_ref else None
+                    }
+
+                    # Include partner data in the response
+                    partner_data = {
+                        'Nama_umkm': partner_ref.get('businessName'),  # Adjust this based on your actual partner document structure
+                    }
+
+                    product_data.update(partner_data)
+
+            else:
+                print(f"Product Document Reference is None")
+
+            combined_data = {
+                **product_data,
+                **user_data
+            }
+            products.append(combined_data)
+
         return jsonify({'status': 'success', 'message': 'products retrieved successfully', 'data': products}), 200
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e), 'data': None}), 500
         
-@app.route('/api/<partner_name>/products/material', methods=['GET'])
-def get_material(partner_name):
-    try:
-        partner_ref = db.collection('verifiedPartner').document(partner_name)
-        partner_snapshot = partner_ref.get()
-        if not partner_snapshot.exists:
-            return jsonify({'status': 'failed', 'message': 'Partner not found', 'data': None}), 404
-        products_collection_ref = partner_ref.collection('products')
-        products_snapshot = products_collection_ref.get()
-        material_data = []
-        for doc in products_snapshot:
-            doc_data = doc.to_dict()
-            material_field = doc_data.get('material')
-            product_name = doc_data.get('name')
-            if material_field and product_name:
-                material_and_name_obj = {'product': product_name, 'material': material_field}
-                material_data.append(material_and_name_obj)
-        return jsonify({'status': 'success', 'message': 'Material retrieved successfully', 'data': material_data}), 200
-    except Exception as e:
-        return jsonify({'status': 'failed', 'message': str(e), 'data': None}), 500
-
-@app.route('/api/<partner_name>/products/category', methods=['GET'])
-def get_category(partner_name):
-    try:
-        partner_ref = db.collection('verifiedPartner').document(partner_name)
-        partner_snapshot = partner_ref.get()
-        if not partner_snapshot.exists:
-            return jsonify({'status': 'failed', 'message': 'Partner not found', 'data': None}), 404
-        products_collection_ref = partner_ref.collection('products')
-        products_snapshot = products_collection_ref.get()
-        category_data = []
-        for doc in products_snapshot:
-            doc_data = doc.to_dict()
-            category_field = doc_data.get('kategori')
-            product_name = doc_data.get('name')
-            if category_field and product_name:
-                category_and_name_obj = {'product': product_name, 'Category': category_field}
-                category_data.append(category_and_name_obj)
-        return jsonify({'status': 'success', 'message': 'Category retrieved successfully', 'data': category_data}), 200
-    except Exception as e:
-        return jsonify({'status': 'failed', 'message': str(e), 'data': None}), 500
-
-
-@app.route('/api/partner', methods=['GET'])
-def get_partner():
-    try:
-        partner_ref = db.collection('verifiedPartner')
-        partner_snapshot = partner_ref.get()
-        if len(partner_snapshot) == 0:
-            return jsonify({'status': 404, 'message': 'Partner not found', 'data': partner_snapshot})
-        partner_data = []
-        for partner in partner_snapshot:
-            data = partner.to_dict()
-            partner_data.append(data)
-        return jsonify({'status': 'success', 'message':'Partner retrieved successfully', 'data': partner_data}), 200
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e), 'data': None}), 500
-
-@app.route('/api/users', methods=['GET'])
-def get_user():
-    try:
-        user_ref = db.collection('users')
-        user_snapshot = user_ref.get()
-        user_data = []
-        for user in user_snapshot:
-            data = user.to_dict()
-            data['id'] = data.pop('uuid')
-            user_data.append(data)
-        return jsonify({'status': 'success', 'message':'Users retrieved successfully', 'data': user_data}), 200
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e), 'data': None}), 500
-
-@app.route('/api/users/<user_email>/history', methods=['GET'])
-def get_history(user_email):
-    try:
-        user_ref = db.collection('users').document(user_email)
-        user_snapshot = user_ref.get()
-        if not user_snapshot.exists:
-            return jsonify({'status': 'failed', 'message': 'User not found', 'data': None}), 404
-        history_collection_ref = user_ref.collection('history')
-        history_snapshot = history_collection_ref.get()
-        history_data = []
-        for history in history_snapshot:
-            data = history.to_dict()
-            if 'transactionRef' in data and data['transactionRef']:
-                transaction_ref_id = data['transactionRef'].id
-                data['transactionRef'] = transaction_ref_id
-                transaction_doc_ref = db.document(f'verifiedPartner/uwg/history/{transaction_ref_id}')
-                transaction_data = transaction_doc_ref.get().to_dict()
-                if transaction_data:
-                    data['transactionData'] = transaction_data
-                    if 'productRef' in transaction_data and transaction_data['productRef']:
-                        product_ref = transaction_data['productRef']
-                        product_doc_ref = db.document(product_ref)
-                        product_data = product_doc_ref.get().to_dict()
-                        data['transactionData']['productData'] = product_data
-                        del data['transactionData']['productRef']
-                else:
-                    data['transactionData'] = None
-            history_data.append(data)
-        return jsonify({'status': 'success', 'message':' History Users retrieved successfully', 'data': history_data}), 200
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e), 'data': None}), 500
-
 if __name__ == '__main__':
     app.run(debug=True)
